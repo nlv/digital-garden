d4231ec3748b91710c255ec00c2497daee240c56
Leon Nikitin <Leon.V.Nikitin@pravmail.ru>
2026-01-05 00:45:19 +0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "ru_RU",
+    locale: "en_US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
