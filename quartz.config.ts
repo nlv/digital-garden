4d563b0b5192be0eabfa40d951827c50c8d9155e
Leon Nikitin <Leon.V.Nikitin@pravmail.ru>
2026-01-05 00:37:54 +0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Цифровой сад Лев Никитин (nlvomsk)",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "ru_ru",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
